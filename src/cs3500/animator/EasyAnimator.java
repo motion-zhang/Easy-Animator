6913ce9fb0949848ab03633b7c04df32e3e39784
Motion-Zhang <zhang.li@husky.neu.edu>
2018-10-19 19:48:03 -0400

    Update EasyAnimator.java

--- src/cs3500/animator/EasyAnimator.java
+++ src/cs3500/animator/EasyAnimator.java
@@ -66,7 +66,7 @@ public final class EasyAnimator {
           break;
 
         default:
-          JOptionPane.showMessageDialog(frame, "Invalid Args");
+          JOptionPane.showMessageDialog(frame, "Invalid Args   ");
           System.exit(1);
           return;
       }
